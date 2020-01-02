1c78beded475b5af8c348de482b16d61a1546bb9
phooky <phooky@gmail.com>
2020-01-02 14:47:38 -0500

    looks like it's working, but want to move to cgmath crate

--- dome_calc/src/main.rs
+++ dome_calc/src/main.rs
@@ -3,11 +3,16 @@ extern crate cairo;
 use cairo::{ SvgSurface, Format, Context };
 
 
-// Icosahedron geometry
+// Icosahedron geometry, all relative to edge length
 const ICO_0R : f64 = 0.9510565; // circum-sphere
 const ICO_1R : f64 = 0.8091699; // mid-sphere
 const ICO_2R : f64 = 0.7557613; // in-sphere
 
+const SQRT_3 : f64 = 1.7320508075688772;
+const TRI_H : f64 = SQRT_3/2.0; // height of triangle with unit edge
+
+const SVG_WIDTH : f64 = 200.0;
+const SVG_HEIGHT : f64 = 200.0;
 
 struct Point { x : f64, y : f64, z : f64 }
 
@@ -20,7 +25,7 @@ struct Panel {
 }
 
 impl Point {
-    fn project( &mut self, r : f64 ) {
+    fn project_to_radius( &mut self, r : f64 ) {
         let origin = Point{ x: 0.0, y : 0.0, z : 0.0 };
         // Check for point at origin?
         let factor = r / distance( &origin, self );
@@ -42,12 +47,17 @@ impl Panel {
         // frequency 0 is a triangle; freq 1 is edges broken in one place, etc.
         let n = (freq+2)*(freq+3) / 2;
         let mut panel = Panel{ n : n, p : Vec::new(), e : Vec::new() };
+        let edges : f64 = (freq + 1) as f64;
+        let x0 : f64 = 0.0;
+        let y0 : f64 = -(TRI_H*2.0/3.0);
+        let rowht = TRI_H / edges;
+        let edgelen = 1.0 / edges;
         for i in 0..freq+2 { // row
+            let leftmost = x0 - (i as f64)*edgelen/2.0;
             for j in 0..i+1 { // point in row
-                let leftmost = -(i as f64) / 2.0;
                 panel.p.push( Point{ 
-                    x : leftmost + j as f64,
-                    y : i as f64,
+                    x : leftmost + edgelen * (j as f64),
+                    y : y0 + rowht * (i as f64),
                     z : ICO_2R } );
             }
         }
@@ -67,16 +77,22 @@ impl Panel {
 }
 
 fn main() {
-    let surface =  SvgSurface::new(600.0,600.0,Some("test.svg")).expect("Couldn't create svgsurface");
+    let surface =  SvgSurface::new(SVG_WIDTH,SVG_HEIGHT,Some("test.svg")).expect("Couldn't create svgsurface");
     let context = Context::new(&surface);
-    let panel = Panel::build(4);
+    let mut panel = Panel::build(4);
     context.set_source_rgb(0.5,0.5,0.5);
     context.paint();
     context.set_source_rgb(0.0,0.0,0.0);
+    for e in &panel.e {
+        let (a,b) = (&panel.p[e.a], &panel.p[e.b]);
+        context.move_to(SVG_WIDTH/2.0 + a.x*100.0, SVG_HEIGHT/2.0 + a.y*100.0);
+        context.line_to(SVG_WIDTH/2.0 + b.x*100.0, SVG_HEIGHT/2.0 + b.y*100.0);
+    }
+    for p in &mut panel.p { p.project_to_radius(ICO_0R); }
     for e in panel.e {
         let (a,b) = (&panel.p[e.a], &panel.p[e.b]);
-        context.move_to(300.0 + a.x*30.0,300.0+a.y*30.0);
-        context.line_to(300.0 + b.x*30.0,300.0+b.y*30.0);
+        context.move_to(SVG_WIDTH/2.0 + a.x*100.0, SVG_HEIGHT/2.0 + a.y*100.0);
+        context.line_to(SVG_WIDTH/2.0 + b.x*100.0, SVG_HEIGHT/2.0 + b.y*100.0);
     }
     context.stroke();
     println!("Hello, world!");
