4fc8a1db6492b8076d1039302ebdb328a407f0de
phooky <phooky@gmail.com>
2020-01-03 14:35:50 -0500

    finish changeover to cgmath lib

--- dome_calc/src/main.rs
+++ dome_calc/src/main.rs
@@ -2,7 +2,8 @@ extern crate cairo;
 extern crate cgmath;
 
 use cairo::{ SvgSurface, Format, Context };
-
+use cgmath::prelude::*;
+use cgmath::{ Point3, Vector3, Matrix4 };
 
 // Icosahedron geometry, all relative to edge length
 const ICO_0R : f64 = 0.9510565; // circum-sphere
@@ -15,7 +16,7 @@ const TRI_H : f64 = SQRT_3/2.0; // height of triangle with unit edge
 const SVG_WIDTH : f64 = 200.0;
 const SVG_HEIGHT : f64 = 200.0;
 
-type Point = cgmath::Point3<f64>;
+type Point = Point3<f64>;
 
 struct Edge { a : usize, b : usize }
 
@@ -25,33 +26,16 @@ struct Panel {
     e : Vec<Edge>
 }
 
-fn project_to_radius( p : &mut Point, r : f64 ) {
-    let origin = Point{ x: 0.0, y : 0.0, z : 0.0 };
-    // Check for point at origin?
-    let factor = r / distance( &origin, p );
-    p.x *= factor;
-    p.y *= factor;
-    p.z *= factor;
-}
-
-fn distance( a : &Point, b : &Point ) -> f64 {
-    let dx = a.x - b.x;
-    let dy = a.y - b.y;
-    let dz = a.z - b.z;
-    ((dx*dx)+(dy*dy)+(dz*dz)).sqrt()
-}
-
 impl Panel {
     fn build(freq : usize) -> Panel {
-        // frequency 0 is a triangle; freq 1 is edges broken in one place, etc.
-        let n = (freq+2)*(freq+3) / 2;
+        // frequency 1 is a triangle; freq 2 is edges broken in one place, etc.
+        let n = (freq+1)*(freq+2) / 2;
         let mut panel = Panel{ n : n, p : Vec::new(), e : Vec::new() };
-        let edges : f64 = (freq + 1) as f64;
         let x0 : f64 = 0.0;
         let y0 : f64 = -(TRI_H*2.0/3.0);
-        let rowht = TRI_H / edges;
-        let edgelen = 1.0 / edges;
-        for i in 0..freq+2 { // row
+        let rowht = TRI_H / freq as f64;
+        let edgelen = 1.0 / freq as f64;
+        for i in 0..freq+1 { // row
             let leftmost = x0 - (i as f64)*edgelen/2.0;
             for j in 0..i+1 { // point in row
                 panel.p.push( Point{ 
@@ -60,7 +44,7 @@ impl Panel {
                     z : ICO_2R } );
             }
         }
-        for i in 0..freq+1 {
+        for i in 0..freq {
             let a_row_idx = i*(i+1) / 2;
             let b_row_idx = (i+1)*(i+2) / 2;
             for j in 0..i+1 {
@@ -82,17 +66,20 @@ fn main() {
     context.set_source_rgb(0.5,0.5,0.5);
     context.paint();
     context.set_source_rgb(0.0,0.0,0.0);
+    let mut t = 
+        Matrix4::from_translation( Vector3 { x : SVG_WIDTH/2.0, y : SVG_HEIGHT/2.0, z : 0.0 } )
+        * Matrix4::from_scale(100.0);
+
     for e in &panel.e {
-        let (a,b) = (&panel.p[e.a], &panel.p[e.b]);
-        context.move_to(SVG_WIDTH/2.0 + a.x*100.0, SVG_HEIGHT/2.0 + a.y*100.0);
-        context.line_to(SVG_WIDTH/2.0 + b.x*100.0, SVG_HEIGHT/2.0 + b.y*100.0);
+        let (a,b) = (t.transform_point(panel.p[e.a]), t.transform_point(panel.p[e.b]));
+        context.move_to(a.x, a.y);
+        context.line_to(b.x, b.y);
     }
-    for p in &mut panel.p { project_to_radius(p,ICO_0R); }
+    for p in &mut panel.p { *p = Point3::from_vec(p.to_vec().normalize_to(ICO_0R)); }
     for e in panel.e {
-        let (a,b) = (&panel.p[e.a], &panel.p[e.b]);
-        context.move_to(SVG_WIDTH/2.0 + a.x*100.0, SVG_HEIGHT/2.0 + a.y*100.0);
-        context.line_to(SVG_WIDTH/2.0 + b.x*100.0, SVG_HEIGHT/2.0 + b.y*100.0);
+        let (a,b) = (t.transform_point(panel.p[e.a]), t.transform_point(panel.p[e.b]));
+        context.move_to(a.x, a.y);
+        context.line_to(b.x, b.y);
     }
     context.stroke();
-    println!("Hello, world!");
 }
