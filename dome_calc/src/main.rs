1caa33e6418dcf18396f6730e6c6e924255edf0a
phooky <phooky@gmail.com>
2020-01-07 10:36:54 -0500

    factor out rendering

--- dome_calc/src/main.rs
+++ dome_calc/src/main.rs
@@ -13,10 +13,19 @@ const ICO_2R : f64 = 0.7557613; // in-sphere
 const SQRT_3 : f64 = 1.7320508075688772;
 const TRI_H : f64 = SQRT_3/2.0; // height of triangle with unit edge
 
-const SVG_WIDTH : f64 = 200.0;
-const SVG_HEIGHT : f64 = 200.0;
+const SVG_WIDTH : f64 = 300.0;
+const SVG_HEIGHT : f64 = 300.0;
 
-type Point = Point3<f64>;
+enum PointType {
+    Ordinary,
+    Corner,
+    Edge,
+}
+
+struct Point {
+    p : Point3<f64>,
+    t : PointType,
+}
 
 struct Edge { a : usize, b : usize }
 
@@ -35,15 +44,26 @@ impl Panel {
         let y0 : f64 = -(TRI_H*2.0/3.0);
         let rowht = TRI_H / freq as f64;
         let edgelen = 1.0 / freq as f64;
+
+        // Create points on panel
         for i in 0..freq+1 { // row
             let leftmost = x0 - (i as f64)*edgelen/2.0;
             for j in 0..i+1 { // point in row
-                panel.p.push( Point{ 
+                panel.p.push( Point{ t : if j == 0 || j == i {
+                    if i == 0 || i == freq {
+                        PointType::Corner } else {
+                        PointType::Edge }
+                } else {
+                    PointType::Ordinary 
+                },
+                p : Point3 { 
                     x : leftmost + edgelen * (j as f64),
                     y : y0 + rowht * (i as f64),
-                    z : ICO_2R } );
+                    z : ICO_2R } } );
             }
         }
+
+        // Add edges
         for i in 0..freq {
             let a_row_idx = i*(i+1) / 2;
             let b_row_idx = (i+1)*(i+2) / 2;
@@ -57,6 +77,21 @@ impl Panel {
         }
         panel
     }
+
+    fn render(&self, context : &Context, m : &Matrix4<f64>) {
+        context.save();
+        context.set_line_cap(cairo::LineCap::Round);
+        context.set_line_width(1.5);
+        for e in &self.e {
+            let a = m.transform_point(self.p[e.a].p);
+            let b = m.transform_point(self.p[e.b].p);
+            context.move_to(a.x, a.y);
+            context.line_to(b.x, b.y);
+        }
+        context.stroke();
+        context.restore();
+    }
+
 }
 
 fn main() {
@@ -70,16 +105,7 @@ fn main() {
         Matrix4::from_translation( Vector3 { x : SVG_WIDTH/2.0, y : SVG_HEIGHT/2.0, z : 0.0 } )
         * Matrix4::from_scale(100.0);
 
-    for e in &panel.e {
-        let (a,b) = (t.transform_point(panel.p[e.a]), t.transform_point(panel.p[e.b]));
-        context.move_to(a.x, a.y);
-        context.line_to(b.x, b.y);
-    }
-    for p in &mut panel.p { *p = Point3::from_vec(p.to_vec().normalize_to(ICO_0R)); }
-    for e in panel.e {
-        let (a,b) = (t.transform_point(panel.p[e.a]), t.transform_point(panel.p[e.b]));
-        context.move_to(a.x, a.y);
-        context.line_to(b.x, b.y);
-    }
-    context.stroke();
+    panel.render(&context, &t);
+    for p in &mut panel.p { p.p = Point3::from_vec(p.p.to_vec().normalize_to(ICO_0R)); }
+    panel.render(&context, &t);
 }
