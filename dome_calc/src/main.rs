9f42010d96027830b2f4f3fb2c56e4bd9dfd4a30
phooky <phooky@gmail.com>
2020-01-08 16:26:55 -0500

    adding annotations to vertices

--- dome_calc/src/main.rs
+++ dome_calc/src/main.rs
@@ -3,7 +3,7 @@ extern crate cgmath;
 
 use cairo::{ SvgSurface, Format, Context };
 use cgmath::prelude::*;
-use cgmath::{ Point3, Vector3, Matrix4 };
+use cgmath::{ Point3, Vector3, Matrix4, Rad };
 
 // Icosahedron geometry, all relative to edge length
 const ICO_0R : f64 = 0.9510565; // circum-sphere
@@ -12,6 +12,7 @@ const ICO_2R : f64 = 0.7557613; // in-sphere
 
 const SQRT_3 : f64 = 1.7320508075688772;
 const TRI_H : f64 = SQRT_3/2.0; // height of triangle with unit edge
+use std::f64::consts::PI;
 
 const SVG_WIDTH : f64 = 300.0;
 const SVG_HEIGHT : f64 = 300.0;
@@ -25,9 +26,13 @@ enum PointType {
 struct Point {
     p : Point3<f64>,
     t : PointType,
+    annotation : Option<u8>,
 }
 
-struct Edge { a : usize, b : usize }
+struct Edge { 
+    a : usize, 
+    b : usize,
+}
 
 struct Panel {
     n : usize,
@@ -49,17 +54,18 @@ impl Panel {
         for i in 0..freq+1 { // row
             let leftmost = x0 - (i as f64)*edgelen/2.0;
             for j in 0..i+1 { // point in row
+                let anno = if j <= freq/2 { Some(1) } else { None };
+                use PointType::*;
                 panel.p.push( Point{ t : if j == 0 || j == i {
-                    if i == 0 || i == freq {
-                        PointType::Corner } else {
-                        PointType::Edge }
+                    if i == 0 || i == freq { Corner } else { Edge }
                 } else {
-                    PointType::Ordinary 
+                    if i == freq { Edge } else { Ordinary }
                 },
                 p : Point3 { 
                     x : leftmost + edgelen * (j as f64),
                     y : y0 + rowht * (i as f64),
-                    z : ICO_2R } } );
+                    z : ICO_2R },
+                annotation : anno } );
             }
         }
 
@@ -81,12 +87,42 @@ impl Panel {
     fn render(&self, context : &Context, m : &Matrix4<f64>) {
         context.save();
         context.set_line_cap(cairo::LineCap::Round);
-        context.set_line_width(1.5);
+        context.set_line_width(1.0);
         for e in &self.e {
             let a = m.transform_point(self.p[e.a].p);
             let b = m.transform_point(self.p[e.b].p);
             context.move_to(a.x, a.y);
             context.line_to(b.x, b.y);
+        }
+        context.stroke();
+        context.set_line_width(0.8);
+        for p in &self.p {
+            let pp = m.transform_point(p.p);
+            use PointType::*;
+            context.arc(pp.x, pp.y, 1.2, 0.0, 2.0*PI);
+            context.fill();
+            match p.annotation {
+                None => {},
+                Some(x) => {
+                    context.set_source_rgb(0.0,0.0,0.8);
+                },
+            }
+
+            match p.t {
+                Ordinary => { },
+                Corner => {
+                    context.new_sub_path();
+                    context.arc(pp.x, pp.y, 2.4, 0.0, 2.0*PI);
+                    context.new_sub_path();
+                    context.arc(pp.x, pp.y, 4.2, 0.0, 2.0*PI);
+                    context.stroke();
+                },
+                Edge => {
+                    context.rectangle(pp.x-2.5,pp.y-2.5,5.0,5.0);
+                    context.stroke();
+                },
+            }
+            context.set_source_rgb(0.0,0.0,0.0);
         }
         context.stroke();
         context.restore();
@@ -98,14 +134,16 @@ fn main() {
     let surface =  SvgSurface::new(SVG_WIDTH,SVG_HEIGHT,Some("test.svg")).expect("Couldn't create svgsurface");
     let context = Context::new(&surface);
     let mut panel = Panel::build(4);
-    context.set_source_rgb(0.5,0.5,0.5);
+    context.set_source_rgb(0.8,0.5,0.5);
     context.paint();
     context.set_source_rgb(0.0,0.0,0.0);
-    let mut t = 
+    let rotm = Matrix4::from_angle_x(Rad(PI/4.0)) * Matrix4::from_angle_z(Rad(PI/8.4));
+    let scrm = 
         Matrix4::from_translation( Vector3 { x : SVG_WIDTH/2.0, y : SVG_HEIGHT/2.0, z : 0.0 } )
-        * Matrix4::from_scale(100.0);
-
+        * Matrix4::from_scale(150.0);
+    let t = scrm * Matrix4::from_translation( Vector3 { x:0.0, y:0.8, z:0.0 }) * rotm;
     panel.render(&context, &t);
     for p in &mut panel.p { p.p = Point3::from_vec(p.p.to_vec().normalize_to(ICO_0R)); }
+    let t = scrm * Matrix4::from_translation( Vector3 { x:0.0, y:0.3, z:0.0 }) * rotm;
     panel.render(&context, &t);
 }
